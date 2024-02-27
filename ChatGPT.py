1074cd4fe2d228007710f3a1819edcbad732b8c0
as176590811 <143897333+as176590811@users.noreply.github.com>
2024-02-28 00:14:36 +0800

    Update ChatGPT.py

--- ChatGPT.py
+++ ChatGPT.py
@@ -188,8 +188,8 @@ def translate():
     text = request.args.get('text')  
     print(f"接收到的文本: {text}")
     #检测text中是否包含"\n",如果包含则替换成\\n  
-    if '\n' in text:  
-        text=text.replace('\n','\\n')  
+    # if '\n' in text:
+    #    text=text.replace('\n','\\n')  
 
     translation_queue = Queue()
     translation_thread = Thread(target=handle_translation, args=(text, translation_queue))
@@ -199,7 +199,7 @@ def translate():
     translation = translation_queue.get()
 
     if translation:
-         translation=translation.replace('\\n','\n')  
+         # translation=translation.replace('\\n','\n')  
          return f"{translation}"
     else:
          return "翻译失败", 500
